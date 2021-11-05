930dc75a26e80ac40f74b2dac2c163833c6dd4a8
yinka <holayinkajr@gmail.com>
2021-11-05 15:05:17 +0100

    other model queries

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -1,4 +1,4 @@
---name: CreateEntry :one
+-- name: CreateEntry :one
 INSERT INTO entries (
     account_id,
     amount
@@ -6,17 +6,14 @@ INSERT INTO entries (
     $1, $2
 ) RETURNING *;
 
---name: GetEntry :one
+-- name: GetEntry :one
 SELECT * FROM entries
 WHERE id = $1
 LIMIT 1;
 
---name: ListEntries :many
+-- name: ListEntries :many
 SELECT * FROM entries
 WHERE account_id = $1
 ORDER BY id
 LIMIT $2
 OFFSET $3;
-
-
-
--- db/query/transfer.sql
+++ db/query/transfer.sql
@@ -1,5 +1,5 @@
---name CreateTransfer :one
-INSERT INRO transfers(
+-- name: CreateTransfer :one
+INSERT INTO transfers(
     from_account_id,
     to_account_id,
     amount
@@ -7,12 +7,12 @@ INSERT INRO transfers(
     $1, $2, $3
 ) RETURNING *;
 
---name: GetTransfer :one
+-- name: GetTransfer :one
 SELECT * FROM transfers
 WHERE id = $1
 LIMIT 1;
 
---name: ListTransfers :many
+-- name: ListTransfers :many
 SELECT * FROM transfers
 WHERE 
     from_account_id = $1 OR
