a5bb8d2cf8248dc0a4378dfcec4542de7bdcfe5d
yinka <holayinkajr@gmail.com>
2021-11-07 18:02:11 +0100

    transfer transaction test

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,75 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	acc1 := createRandomAccount(t)
+	acc2 := createRandomAccount(t)
+
+	n := 5
+	amount := int64(10)
+	errsChan := make(chan error)
+	resultsChan := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: acc1.ID,
+				ToAccountID:   acc2.ID,
+				Amount:        amount,
+			})
+			errsChan <- err
+			resultsChan <- result
+		}()
+	}
+	for i := 0; i < n; i++ {
+		err := <-errsChan
+		require.NoError(t, err)
+
+		result := <-resultsChan
+		require.NotEmpty(t, result)
+
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, acc1.ID, transfer.FromAccountID)
+		require.Equal(t, acc2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		for i := 0; i < n; i++ {
+			_, err := store.GetTransfer(context.Background(), transfer.ID)
+			require.NoError(t, err)
+		}
+
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, acc1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, acc2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		//test checking accounts' balance
+	}
+
+}
