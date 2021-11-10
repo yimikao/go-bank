d74404021d19c081f9cff9ec7a093aa7d916d132
yinka <holayinkajr@gmail.com>
2021-11-10 17:12:38 +0100

    read config from file and env variables with Viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,26 +2,27 @@ package db
 
 import (
 	"database/sql"
+	"gobank/util"
 	"log"
 	"os"
 	"testing"
-
-	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbURI    = "postgres://yinka:supadiski@localhost:5432/go_bank?sslmode=disable"
+	// _ "github.com/lib/pq"
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbURI)
+	cfg, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(cfg.DBDriver, cfg.DBSource)
 
 	if err != nil {
 		log.Fatalf("can't connect to db: %s", err)
 	}
-	testQueries = New(conn)
+	testQueries = New(testDB)
 	os.Exit(m.Run())
 }
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"gobank/api"
+	db "gobank/db/sqlc"
+	"gobank/util"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+	cfg, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(cfg.DBDriver, cfg.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(cfg.ServerAddr)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
