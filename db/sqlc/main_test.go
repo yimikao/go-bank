7174c3068dcbf28447389f4af8d2116cb94d1183
yinka <holayinkajr@gmail.com>
2022-01-05 12:57:08 +0100

    updated comments

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,9 +9,10 @@ import (
 	// _ "github.com/lib/pq"
 )
 
-var testQueries *Queries
+var testQueries *Queries //A test Queries object(Test Database wrapping either sql.DB/Tx)
 var testDB *sql.DB
 
+// main entry point of all unit tests inside 1 specific package
 func TestMain(m *testing.M) {
 	cfg, err := util.LoadConfig("../..")
 	if err != nil {
