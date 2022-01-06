c614f0eeeed96e74715aa57dd952ea15a1878f1e
yinka <holayinkajr@gmail.com>
2022-01-06 20:09:19 +0100

    add foreign key constraint to test and fix user not returning the right error  status code

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -27,6 +28,13 @@ func (s *Server) createAccount(ctx *gin.Context) {
 	}
 	acc, err := s.store.CreateAccount(ctx, args)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
