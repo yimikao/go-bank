f13a66d600d998df5350b59e1219bd40ea1eb7ad
yinka <holayinkajr@gmail.com>
2022-01-06 20:41:37 +0100

    use HashPassword function

--- util/password_test.go
+++ util/password_test.go
@@ -9,14 +9,19 @@ import (
 func TestPassword(t *testing.T) {
 	pwd := RandomString(6)
 
-	hashedPwd, err := HashPassword(pwd)
+	hashedPwd1, err := HashPassword(pwd)
 	require.NoError(t, err)
-	require.NotEmpty(t, hashedPwd)
+	require.NotEmpty(t, hashedPwd1)
 
-	err = CheckPassword(pwd, hashedPwd)
+	err = CheckPassword(pwd, hashedPwd1)
 	require.NoError(t, err)
 
 	// wrongPassword := RandomString(6)
 	// err = CheckPassword(wrongPassword, hashedPassword)
 	// require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	hashedPwd2, err := HashPassword(pwd)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPwd2)
+	require.NotEqual(t, hashedPwd1, hashedPwd2)
 }
