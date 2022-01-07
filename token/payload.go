32dd2431c519d13d46e22e959fa37c3eedbf6ecf
yinka <holayinkajr@gmail.com>
2022-01-07 20:40:33 +0100

    payload(claims) modelling

---
+++ token/payload.go
@@ -0,0 +1,45 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+var (
+	ErrInvalidToken = errors.New("token is invalid!")
+	ErrExpiredToken = errors.New("token has expired!")
+)
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
+
+//this valid method must be attached to payload to satisfy
+// Claims interface(needed by jwt.NewWithClaims())
+
+func (p *Payload) Valid() error {
+	if time.Now().After(p.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
