163a9a8592b5cb10e56dcdf48e65ebd3e7a37386
yinka <holayinkajr@gmail.com>
2022-01-07 20:16:51 +0100

    handle postgres db errors correctly

--- api/account.go
+++ api/account.go
@@ -30,6 +30,7 @@ func (s *Server) createAccount(ctx *gin.Context) {
 	if err != nil {
 		if pqErr, ok := err.(*pq.Error); ok {
 			switch pqErr.Code.Name() {
+			//2 constraints in accs table: FK owner, UNIQUE owner currency
 			case "foreign_key_violation", "unique_violation":
 				ctx.JSON(http.StatusForbidden, errorResponse(err))
 				return
