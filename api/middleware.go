4ae031e65c8fd184ea3eb367a44d88e352524094
yinka <holayinkajr@gmail.com>
2022-01-31 22:46:57 +0100

    created authorization middleware

---
+++ api/middleware.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"errors"
+	"gobank/token"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	authorizationTypeOauth  = "oauth"
+	authorizationPayloadKey = "authToken"
+)
+
+func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		authHeader := ctx.GetHeader(authorizationHeaderKey)
+		if len(authHeader) == 0 {
+			err := errors.New("authorization header not provided")
+			ctx.AbortWithStatusJSON(http.StatusForbidden, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authHeader)
+		if len(fields) < 2 {
+			err := errors.New("wrong authorization header format provided")
+			ctx.AbortWithStatusJSON(http.StatusForbidden, errorResponse(err))
+			return
+		}
+
+		if strings.ToLower(fields[0]) != authorizationTypeBearer {
+			err := errors.New("wrong autoricxation header type provided")
+			ctx.AbortWithStatusJSON(http.StatusForbidden, errorResponse(err))
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusForbidden, errorResponse(err))
+			return
+		}
+
+		ctx.Set(authorizationPayloadKey, payload)
+		ctx.Next()
+
+	}
+}
