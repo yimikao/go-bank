b24cda57e0fc018226c474b60e58667b11e38345
yinka <holayinkajr@gmail.com>
2022-02-03 15:42:27 +0100

    let only auth'ed user be able to make transfer from his/her account

--- api/transfer.go
+++ api/transfer.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	db "gobank/db/sqlc"
+	"gobank/token"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,6 +16,23 @@ type transferRequest struct {
 	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
 	Amount        int64  `json:"amount" binding:"required,gt=0"`
 	Currency      string `json:"currency" binding:"required,currency"`
+}
+
+func (s *Server) validAccount(ctx *gin.Context, accountID int64, currency string) (db.Account, bool) {
+	acc, err := s.store.GetAccount(ctx, accountID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return acc, false
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+	if acc.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", acc.ID, acc.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return acc, false
+	}
+	return acc, true
 }
 
 func (s *Server) createTransfer(ctx *gin.Context) {
@@ -25,11 +46,20 @@ func (s *Server) createTransfer(ctx *gin.Context) {
 		ToAccountID:   req.ToAccountID,
 		Amount:        req.Amount,
 	}
-	isValid := s.validAccount(ctx, args.FromAccountID, req.Currency)
+	account, isValid := s.validAccount(ctx, args.FromAccountID, req.Currency)
 	if !isValid {
 		return
 	}
-	isValid = s.validAccount(ctx, args.ToAccountID, req.Currency)
+
+	payload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+
+	if payload.Username != account.Owner {
+		err := errors.New("from account doesn't belong to authenticated user. can only make transfer from account you own")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	_, isValid = s.validAccount(ctx, args.ToAccountID, req.Currency)
 	if !isValid {
 		return
 	}
